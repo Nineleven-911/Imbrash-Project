d92b0d59236f8066d1f9a25a65c2d0fcb0f9e2cd
AW <mc.20121112.bd@outlook.com>
2025-06-17 17:02:44 +0800

    Optimization(IVM): 重构虚拟机执行单元和堆栈
    - 优化了 ExecutionUnit 类的执行逻辑- 重构了 VirtualMachineStack 类，增加了新的方法
    - 添加了测试代码以验证虚拟机功能

--- Imbrash-VM/src/main/kotlin/Main.kt
+++ Imbrash-VM/src/main/kotlin/Main.kt
@@ -1,4 +1,47 @@
 package hairinne.ip.vm
 
+import hairinne.ip.vm.code.Bytecode
+import hairinne.ip.vm.code.Module
+import hairinne.ip.vm.vm.ExecutionUnit
+
 fun main() {
+    println("""
+      TestCode:
+        push 1
+        push 2
+        push 3
+        push 4
+        prt
+        push 5
+        prt
+        pop
+        prt
+        pop
+        prt
+        pop
+        prt
+        ret
+    """.trimIndent())
+    val executionUnit = ExecutionUnit(
+        Module(
+            mutableListOf(
+                Bytecode.PUSH, 0, 1,
+                Bytecode.PUSH, 0, 2,
+                Bytecode.PUSH, 0, 3,
+                Bytecode.PUSH, 0, 4,
+                Bytecode.PRT, 0,
+                Bytecode.PUSH, 0, 5,
+                Bytecode.PRT, 0,
+                Bytecode.POP, 0,
+                Bytecode.PRT, 0,
+                Bytecode.POP, 0,
+                Bytecode.PRT, 0,
+                Bytecode.POP, 0,
+                Bytecode.PRT, 0,
+                Bytecode.RET, 0
+            )
+        ),
+        arrayOf()
+    )
+    executionUnit.execute()
 }
--- Imbrash-VM/src/main/kotlin/stack/VirtualMachineStack.kt
+++ Imbrash-VM/src/main/kotlin/stack/VirtualMachineStack.kt
@@ -1,11 +1,11 @@
 package hairinne.ip.vm.stack
 
 class VirtualMachineStack {
-    var stack: ByteArray = ByteArray(1024)
+    var stack: ByteArray = ByteArray(128)
     var ptr: Int = 0
 
     fun expandable() {
-        if (ptr + 10 >= stack.size) {
+        if (ptr + 64 >= stack.size) {
             val tmp = ByteArray(stack.size shl 1)
             System.arraycopy(stack, 0, tmp, 0, stack.size)
             stack = tmp
@@ -22,15 +22,47 @@ class VirtualMachineStack {
 
     fun push(value: Byte) { // 只压入一个字节
         stack[ptr++] = value
-        expandable()
+        // expandable()
     }
 
     fun pop(): Byte {
         if (ptr > 0) {
-            val value = stack[ptr - 1]
+            val value = stack[ptr--]
             shrinkable()
             return value
         }
         return 0
+    }
+
+    fun get(index: Int = ptr): Byte {
+        return stack[index - 1]
+    }
+
+    /**
+     * Get stack values
+     * @param a VirtualMachineStack
+     * @param size Count of bytes
+     * @return ByteArray
+     */
+    fun getStackValues(size: Int): ByteArray {
+        val arguments = ByteArray(size)
+        for (i in (0 until size).reversed()) {
+            arguments[i] = this.get(
+                this.ptr - i
+            )
+        }
+        return arguments
+    }
+
+    /**
+     * Copy stack values from a to b
+     * @param a VirtualMachineStack (From)
+     * @param b VirtualMachineStack (To)
+     * @param size Count of bytes
+     */
+    fun copyStackValues(to: VirtualMachineStack, size: Int) {
+        for (byte in this.getStackValues(size)) {
+            to.push(byte)
+        }
     }
 }
